91a5fed7a690f23f8a64a9b01aa1b827abc14f90
qtgye <buquia.jace@gmail.com>
2021-04-29 14:38:38 +0800

    Move jest config, update build yaml

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,56 @@
 module.exports = {
+    "roots": [
+        "<rootDir>/src"
+    ],
+    "collectCoverageFrom": [
+        "src/**/*.{js,jsx,ts,tsx}"
+    ],
+    "setupFiles": [
+        "react-app-polyfill/jsdom"
+    ],
+    "setupFilesAfterEnv": [
+        "<rootDir>/src/setupTests.js"
+    ],
+    "testMatch": [
+        "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
+        "<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}"
+    ],
+    "testEnvironment": "jsdom",
+    "testRunner": "/Users/jaysonbuquia/Jace/personal/proj/ifind/node_modules/jest-circus/runner.js",
+    "transform": {
+        "^.+\\.(js|jsx|mjs|cjs|ts|tsx)$": "<rootDir>/config/jest/babelTransform.js",
+        "^.+\\.css$": "<rootDir>/config/jest/cssTransform.js",
+        "^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)": "<rootDir>/config/jest/fileTransform.js"
+    },
+    "transformIgnorePatterns": [
+        "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$",
+        "^.+\\.module\\.(css|sass|scss)$"
+    ],
+    "modulePaths": [],
+    "moduleFileExtensions": [
+        "web.js",
+        "js",
+        "web.ts",
+        "ts",
+        "web.tsx",
+        "tsx",
+        "json",
+        "web.jsx",
+        "jsx",
+        "node"
+    ],
+    "watchPlugins": [
+        "jest-watch-typeahead/filename",
+        "jest-watch-typeahead/testname"
+    ],
+    "resetMocks": true,
     moduleNameMapper: {
-
+        "^react-native$": "react-native-web",
+        "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
+        
         /**
-         * should match webpack aliases from paths.js
-         */
+        * should match webpack aliases from paths.js
+        */
         '^@components/(.*)$': '<rootDir>/src/components/$1',
         '^@templates/(.*)$': '<rootDir>/src/templates/$1',
         '^@pages/(.*)$': '<rootDir>/src/pages/$1',
@@ -11,10 +58,10 @@ module.exports = {
         '^@config/(.*)$': '<rootDir>/src/config/$1',
         '^@mocks/(.*)$': '<rootDir>/src/mocks/$1',
         '^@bootstrap(/.*)?$': '<rootDir>/node_modules/bootstrap/scss/bootstrap',
-
+        
         /**
-         * Mocks css imports
-         */
+        * Mocks css imports
+        */
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/config/jest/fileTransform.js",
         "\\.(css|less|scss|sass)$": "identity-obj-proxy"
     }
