edf9db8aa6989858441d350e4c675cadfa39382b
d4ybr3ak <danamber.gasapo@gmail.com>
2021-05-04 13:55:03 +0800

    modified some styles

--- front-end/src/pages/AGB/index.js
+++ front-end/src/pages/AGB/index.js
@@ -2,10 +2,12 @@ import BasicPage from '@templates/BasicPage';
 import content from '@mocks/pages/agb';
 import { withComponentName } from '@utilities/component';
 
-const AGB = () => (
-    <BasicPage title='AGB'>
-        <div dangerouslySetInnerHTML={{ __html: content }}></div>
-    </BasicPage>
-);
+const AGB = () => {
+    return (
+        <BasicPage title='AGB'>
+            <div dangerouslySetInnerHTML={{ __html: content }}></div>
+        </BasicPage>
+    )
+};
 
 export default withComponentName('AGBPage')(AGB);
