f4bb985caeb93e96a28fe3729b3082d03feeec76
qtgye <buquia.jace@gmail.com>
2021-05-11 13:54:25 +0800

    Replace jsdom with puppeteer

--- api/src/sources/amazon/index.js
+++ api/src/sources/amazon/index.js
@@ -1,50 +1,53 @@
-const { JSDOM } = require('jsdom');
-const fetch = require('node-fetch');
-const BASE_URL = 'https://www.amazon.de';
+import { JSDOM } from 'jsdom';
+import fetch from 'node-fetch';
+import puppeteer from 'puppeteer';
 
+const BASE_URL = 'https://www.amazon.de';
 const buildFullURL = (relativeURL) => `${BASE_URL}${relativeURL}`;
 const buildBestSellersURL = (category) => buildFullURL(`/gp/bestsellers/${category || ''}`);
 
+
+
+
+
 const getBestSellers = async (category) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
   const url = buildBestSellersURL(category);
-  const resp = await fetch(url);
-  const body = await resp.text();
-  const { window } = new JSDOM(body);
-  const { document } = window;
+  const itemsSelector = '#zg-ordered-list .a-list-item';
 
-  const [...items] = document.querySelectorAll('#zg-ordered-list .a-list-item');
+  await page.goto(url);
+  await page.waitForSelector(itemsSelector);
+  const items = await page.$$(itemsSelector);
 
-  console.log({ url, items });
-
-  const exractImageElementData = (imageElement) => {
+  const itemsData = await Promise.all(items.map(async (item) => item.evaluate( (element, BASE_URL) => {
+    const link = element.querySelector('a');
+    const title = link.textContent.trim();
+    const detailURL = `${BASE_URL}${link.href}`;
+    const imageElement = element.querySelector('img');
     const alt = imageElement.getAttribute('alt');
     const src = imageElement.getAttribute('src');
 
     return {
-      alt,
-      sizes: {
-        small: src.replace(/AC_UL200_SR200,200/, 'AC_UL500_SR500,500'),
-        thumbnail: src.replace(/AC_UL200_SR200,200/, 'AC_UL800_SR800,800'),
-        large: src.replace(/AC_UL200_SR200,200/, 'AC_UL120000_SR120000,120000'),
-      }
-    };
-  }
-
-  return Promise.all(items.slice(0, 5).map(async item => {
-    const link = item.querySelector('a');
-    const title = link.textContent.trim();
-    const detailURL = buildFullURL(link.href);
-    const image = exractImageElementData(item.querySelector('img'));
-
-    return {
       // // id: String
-      image,
       title,
-      detailURL
+      detailURL,
+      image: {
+        alt,
+        sizes: {
+          small: src.replace(/AC_UL200_SR200,200/, 'AC_UL500_SR500,500'),
+          thumbnail: src.replace(/AC_UL200_SR200,200/, 'AC_UL800_SR800,800'),
+          large: src.replace(/AC_UL200_SR200,200/, 'AC_UL120000_SR120000,120000'),
+        }
+      }
     }
-  }))
+  }, BASE_URL)));
+
+  await browser.close();
+
+  return itemsData;
 }
 
-module.exports = {
-  getBestSellers,
-};
+export {
+  getBestSellers
+}
