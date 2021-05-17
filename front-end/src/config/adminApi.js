c63d2d486e1c518c0c5f0c1b59d5059fe2d5d712
qtgye <buquia.jace@gmail.com>
2021-05-17 19:19:13 +0800

    Admin Layer - Integrate FE and build updates

---
+++ front-end/src/config/adminApi.js
@@ -0,0 +1,1 @@
+export const ADMIN_API_ROOT = document.querySelector('[name="admin_api_root"]')?.content;
---
+++ front-end/src/contexts/globalDataContext.js
@@ -0,0 +1,42 @@
+import { createContext, useContext, useEffect, useReducer, useCallback } from 'react';
+import { useQuery } from '@apollo/react-hooks';
+
+import { locale } from '@config/locale';
+import globalDataQuery from '@gql/globalDataQuery';
+
+/**
+ * This context should contain global data:
+ * - Site data (contact infos, logo, etc.)
+ * - Navigation items ?
+ * - Footer items ?
+ */
+const GlobalContext = createContext({});
+
+export const GlobalContextProvider = ({ children }) => {
+    const {
+        // loading,
+        // error,
+        data: globalData
+    } = useQuery(globalDataQuery, {
+        variables: { locale },
+        context: { apiSource: 'admin' }
+    });
+
+    return (
+        <GlobalContext.Provider value={{
+            contactInfo: globalData?.contactDetail,
+            footerSetting: globalData?.footerSetting,
+            socialNetwork: globalData?.socialNetwork
+        }}>
+            {children}
+        </GlobalContext.Provider>
+    );
+}
+
+export const useGlobalData = () => {
+    const context = useContext(GlobalContext);
+    return context;
+}
+
+// Export as default to be used in testing
+export default GlobalContext;
