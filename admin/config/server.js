653e40607cef458e5fca9a61b306a8a3bea2e51f
qtgye <buquia.jace@gmail.com>
2021-05-18 11:00:29 +0800

    Admin - Update FE endpoint and server config

--- admin/config/server.js
+++ admin/config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   admin: {
+    autoOpen: false,
     auth: {
       secret: env('ADMIN_JWT_SECRET', '53f8fce78c5937f78aad791569dc0d60'),
     },
