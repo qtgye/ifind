ef505f84f68395010c97534eb656525839249ef3
qtgye <buquia.jace@gmail.com>
2021-07-23 21:52:33 +0800

    API - Setup API endpoint for eBay verification

--- api/src/server.js
+++ api/src/server.js
@@ -1,36 +1,56 @@
 import express from 'express';
-import { ApolloServer, gql } from 'apollo-server-express';
+// import { ApolloServer, gql } from 'apollo-server-express';
 import minimist from 'minimist';
+import { createHash } from 'crypto';
 
 import config from './config';
-import typeDefs from '@types';
-import resolvers from '@resolvers';
+// import typeDefs from '@types';
+// import resolvers from '@resolvers';
 
 const args = minimist(process.argv);
 
 const port = args.port ? args.port : config.port;
-const path = '/graphql';
+// const path = '/graphql';
 
 // Initialize the app
 const app = express();
 
 // GraphQL Playground
-const playgroundEndpoint = args.apiRoot ? args.apiRoot : `/graphql`;
+// const playgroundEndpoint = args.apiRoot ? args.apiRoot : `/graphql`;
 
-// Initialize Appolo Server
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  introspection: true,
-  playground: {
-    endpoint: playgroundEndpoint
-  }
+// // Initialize Appolo Server
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   introspection: true,
+//   playground: {
+//     endpoint: playgroundEndpoint
+//   }
+// });
+
+// server.applyMiddleware({ app, path });
+
+app.get(config.ebay.marketplaceEndpoint, (req, res) => {
+  // Create a hash for
+  // eBay Marketplace Account Deletion/Closure Notifications
+  // https://developer.ebay.com/marketplace-account-deletion
+  const hash = createHash('sha256');
+  const endpoint = `https://ifindilu.com/api${config.ebay.marketplaceEndpoint}`;
+  const { verificationToken } = config.ebay;
+  const { challengeCode } = req.query;
+
+  hash.update(String(challengeCode));
+  hash.update(verificationToken);
+  hash.update(endpoint);
+
+  const responseHash = hash.digest('hex');
+  const challengeResponse = new Buffer.from(responseHash).toString();
+
+  res.json({ challengeResponse });
 });
-
-server.applyMiddleware({ app, path });
 
 app
 .listen(port, () => {
-  console.info(`ApolloServer now runs at http://localhost:${port}`);
+  console.info(`API server now runs at http://localhost:${port}`);
 });
 
