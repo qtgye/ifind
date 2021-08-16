ef00e12b757dbed572dbabbff0c1315b61b9e433
qtgye <buquia.jace@gmail.com>
2021-08-16 16:14:17 +0800

    Integration - Optimizing Product Comparison Requests

--- front-end/src/contexts/productContext.js
+++ front-end/src/contexts/productContext.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useCallback, useState } from 'react';
-import { useQuery, useMutation } from "@apollo/react-hooks";
+import { useMutation, useLazyQuery } from "@apollo/react-hooks";
 import { apiSourceHandle } from '@config/adminApi'
 import getProductDetailQuery from '@gql/getProductDetailQuery';
 import incrementProductClickMutation from '@gql/incrementProductClickMutation';
@@ -10,29 +10,30 @@ export const ProductContext = createContext({});
 
 export const ProductContextProvider = ({ children }) => {
     const [ productDetail, setProductDetail ] = useState(null);
-    const [ productID, setProductID ] = useState('');
-    const {
-        // loading,
-        // error,
-        data,
-        refetch
-    } = useQuery(getProductDetailQuery, {
-        context: {
-            apiSource: apiSourceHandle,
-        },
-        variables: { 
-            id: productID,
-            language: locale,
+    const [
+        fetchProductDetails,
+        {
+            // loading,
+            error,
+            data,
         }
-    });
+    ] = useLazyQuery(getProductDetailQuery);
     const [
         mutationFunction,
         // { data: mutationData, loading, error },
     ] = useMutation(incrementProductClickMutation);
 
-    const fetchProductDetail = useCallback(( productID ) => {
-        setProductID(productID);
-    }, [ setProductID ]);
+    const getProductDetails = useCallback(( productID ) => {
+        fetchProductDetails({
+            context: {
+                apiSource: apiSourceHandle,
+            },
+            variables: { 
+                id: productID,
+                language: locale,
+            }
+        });
+    }, [ fetchProductDetails ]);
 
     const incrementProductClick = useCallback((id) => {
         mutationFunction({
@@ -44,50 +45,25 @@ export const ProductContextProvider = ({ children }) => {
     }, [ mutationFunction ]);
 
     useEffect(() => {
-        refetch();
-    }, [ productID, refetch ]);
-
-    useEffect(() => {
         setProductDetail(data?.productDetails);
     }, [ data ]);
+
+    useEffect(() => {
+        if ( error ) {
+            console.warn(error);
+        }
+    }, [error])
 
     return (
         <ProductContext.Provider value={{
             productDetail,
-            fetchProductDetail,
+            getProductDetails,
             incrementProductClick,
         }}>
             {children}
         </ProductContext.Provider>
     )
 }
-
-export const useFetchProductDetail = (productID) => {
-    const {
-        // loading,
-        // error,
-        data,
-        refetch
-    } = useQuery(getProductDetailQuery, {
-        context: {
-            apiSource: apiSourceHandle,
-        },
-        variables: { 
-            id: productID,
-            language: locale,
-        }
-    });
-    const [ productDetail, setProductDetail ] = useState(null);
-
-    useEffect(() => {
-        setProductDetail(data?.productDetails);
-    }, [ data ]);
-    
-    return {
-        productDetail,
-        refetchProductDetail: refetch
-    };
-};
 
 export const useProductDetail = () => {
     const context = useContext(ProductContext);
