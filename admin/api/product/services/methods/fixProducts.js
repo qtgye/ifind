835fd4ef38ca7fb3a75c44d5195ff44a6eb2c0fa
qtgye <buquia.jace@gmail.com>
2021-08-18 10:46:38 +0800

    Admin - Optimize Product Fixer

--- admin/api/product/services/methods/fixProducts.js
+++ admin/api/product/services/methods/fixProducts.js
@@ -10,81 +10,80 @@ module.exports = async () => {
   const allProducts = await strapi.services.product.find({ _limit: 99999 });
   const productsWithProblems = filterProductsWithProblems(allProducts);
 
-  return await Promise.all(
-    // Extract and set fixed data for each product
-    productsWithProblems
-      .map((product) => {
-        const productChanges = product.product_changes || [];
+  // Extract and set fixed data for each product
+  const updatedProductsData = productsWithProblems.map((product) => {
+    const productChanges = product.product_changes || [];
 
-        // Sort from recent changes
-        productChanges.sort((changeA, changeB) =>
-          changeA.date_time >= changeB.date_time ? -1 : 1
+    // Sort from recent changes
+    productChanges.sort((changeA, changeB) =>
+      changeA.date_time >= changeB.date_time ? -1 : 1
+    );
+
+    // Fix amazon_url
+    if (!isAmazonLink(product.amazon_url)) {
+      const changeWithAmazonURL = productChanges.find(
+        ({ state }) => state && isAmazonLink(state.amazon_url)
+      );
+      // Apply old valid amazon url
+      if (changeWithAmazonURL) {
+        product.amazon_url = changeWithAmazonURL.state.amazon_url;
+      }
+    }
+    // Fix url_list
+    if (!product.url_list || !product.url_list.length) {
+      const changeWithURLList = productChanges.find(
+        ({ state }) => state && state.url_list && state.url_list.length
+      );
+      // Apply old url list if any
+      if (changeWithURLList) {
+        product.url_list = changeWithURLList.state.url_list;
+      }
+    }
+    // Fix attrs_rating
+    if (!product.attrs_rating || !product.attrs_rating.length) {
+      const changeWithAttrsRating = productChanges.find(
+        ({ state }) => state && state.attrs_rating && state.attrs_rating.length
+      );
+      // Apply old url list if any
+      if (changeWithAttrsRating) {
+        product.attrs_rating = changeWithAttrsRating.state.attrs_rating.map(
+          ({ id, ...attr_rating }) => attr_rating
         );
+      }
+    }
 
-        // Fix amazon_url
-        if (!isAmazonLink(product.amazon_url)) {
-          const changeWithAmazonURL = productChanges.find(
-            ({ state }) => state && isAmazonLink(state.amazon_url)
-          );
-          // Apply old valid amazon url
-          if (changeWithAmazonURL) {
-            product.amazon_url = changeWithAmazonURL.state.amazon_url;
-          }
-        }
-        // Fix url_list
-        if (!product.url_list || !product.url_list.length) {
-          const changeWithURLList = productChanges.find(
-            ({ state }) => state && state.url_list && state.url_list.length
-          );
-          // Apply old url list if any
-          if (changeWithURLList) {
-            product.url_list = changeWithURLList.state.url_list;
-          }
-        }
-        // Fix attrs_rating
-        if (!product.attrs_rating || !product.attrs_rating.length) {
-          const changeWithAttrsRating = productChanges.find(
-            ({ state }) =>
-              state && state.attrs_rating && state.attrs_rating.length
-          );
-          // Apply old url list if any
-          if (changeWithAttrsRating) {
-            product.attrs_rating = changeWithAttrsRating.state.attrs_rating.map(
-              ({ id, ...attr_rating }) => attr_rating
-            );
-          }
-        }
+    // Only apply updates to selected properties
+    return {
+      id: product.id,
+      amazon_url: product.amazon_url,
+      url_list: product.url_list,
+      attrs_rating: product.attrs_rating,
+    };
+  });
 
-        // Only apply updates to selected properties
-        return {
-          id: product.id,
-          amazon_url: product.amazon_url,
-          url_list: product.url_list,
-          attrs_rating: product.attrs_rating,
-        };
-      })
-      // Then, save all these updated products,
-      // Returning the full data for each product
-      .map(async (productUpdates) => {
-        const { id, ...productData } = productUpdates;
+  // Then, save all these updated products,
+  // Returning the full data for each product
+  const savedProducts = [];
 
-        // Prevent lifecycle from scraping
-        productData.updateScope = {
-          price: false,
-          amazonDetails: false,
-        };
+  for (const newData of updatedProductsData) {
+    const { id, ...productData } = newData;
 
-        try {
-          const result = await strapi.services.product.update(
-            { id },
-            productData
-          );
-          console.log(`Saved ${id}`.green.bold, result.title);
-          return result;
-        } catch (err) {
-          console.log(`Error in ${id}`.bgRed.white.bold, productData);
-          console.error(err);
-        }
-      })
-  );
+    // Prevent lifecycle from scraping
+    productData.updateScope = {
+      price: false,
+      amazonDetails: false,
+    };
+
+    try {
+      const result = await strapi.services.product.update({ id }, productData);
+      console.log(`Saved ${id}`.green.bold, result.title);
+      savedProducts.push(result);
+      return result;
+    } catch (err) {
+      console.log(`Error in ${id}`.bgRed.white.bold, productData);
+      console.error(err);
+    }
+  }
+
+  return savedProducts;
 };
