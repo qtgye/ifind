cddbe75d4926400bf93a8394b670f585ac90f373
qtgye <buquia.jace@gmail.com>
2021-10-06 15:20:20 +0800

    Admin - Remove useHistory from ButtonLink

--- admin/plugins/ifind/admin/src/components/ButtonLink/index.tsx
+++ admin/plugins/ifind/admin/src/components/ButtonLink/index.tsx
@@ -1,43 +1,35 @@
-import React, { AnchorHTMLAttributes, useCallback, MouseEventHandler, MouseEvent } from 'react';
-import { useHistory } from 'react-router';
+import React, { AnchorHTMLAttributes } from "react";
 
-import './styles.scss';
+import "./styles.scss";
 
 // TYPES
 export enum E_ButtonLinkColor {
-  primary = 'primary',
-  secondary = 'secondary',
-  cancel = 'cancel',
-  success = 'success',
-  delete = 'delete',
-  none = 'none',
-};
+  primary = "primary",
+  secondary = "secondary",
+  cancel = "cancel",
+  success = "success",
+  delete = "delete",
+  none = "none",
+}
 
 export interface I_ButtonLinkProps extends AnchorHTMLAttributes<any> {
-  color?: E_ButtonLinkColor
+  color?: E_ButtonLinkColor;
 }
 
 // COMPONENT
-const ButtonLink = ({ href = '', color = E_ButtonLinkColor.primary, children = null, ...props }: I_ButtonLinkProps): JSX.Element => {
-  const history = useHistory();
-  
-  const className = [
-    'button-link',
-    `button-link--${color}`,
-  ];
-
-  const onClick = useCallback<MouseEventHandler<HTMLAnchorElement>>((e: MouseEvent) => {
-    e.preventDefault();
-
-    // Prevents page reload
-    history.push(href);
-  }, [ href ]);
+const ButtonLink = ({
+  href = "",
+  color = E_ButtonLinkColor.primary,
+  children = null,
+  ...props
+}: I_ButtonLinkProps): JSX.Element => {
+  const className = ["button-link", `button-link--${color}`];
 
   return (
-    <a href={href} className={className.join(' ')} {...props} onClick={onClick}>
+    <a href={href} className={className.join(" ")} {...props}>
       {children}
     </a>
   );
-}
+};
 
 export default ButtonLink;
