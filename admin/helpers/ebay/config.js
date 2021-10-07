dae1adb70bff19e5f7ea7f12c01997c8103168b8
qtgye <buquia.jace@gmail.com>
2021-10-07 10:57:07 +0800

    Admin - Update Ebay API

--- admin/helpers/ebay/config.js
+++ admin/helpers/ebay/config.js
@@ -1,5 +1,8 @@
 module.exports = {
+  clientId: 'KirillKr-ifindilu-PRD-9afbfbe27-8527205b',
+  clientSecret: 'PRD-afbfbe270089-d729-4240-bbc8-e3b8',
   appId: 'KirillKr-ifindilu-PRD-9afbfbe27-8527205b&',
+  redirectUri: 'https://www.ifindilu.com',
   affiliateParams: {
     campid: '5338787715',
     mkevt: 1,
