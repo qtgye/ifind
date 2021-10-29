67036db9ddc42cc290cb7e3921d86a7010bbdd2f
qtgye <buquia.jace@gmail.com>
2021-10-29 20:28:28 +0800

    Admin - Restructure Product Validator Task

--- admin/background-process/product-validator/index.js
+++ admin/background-process/product-validator/index.js
@@ -1,7 +1,4 @@
 require("colors");
-const path = require("path");
-const args = require("minimist")(process.argv.slice(2));
-
 require("../../helpers/customGlobals");
 
 const adminStrapi = appRequire("scripts/strapi-custom");
@@ -10,220 +7,119 @@ const { getDetailsFromURL: getDetailsFromEbayURL } = appRequire("helpers/ebay");
 const { getDetailsFromURL: getDetailsFromAliExppressURL } =
   appRequire("helpers/aliexpress");
 
-const BackgroundProcess = require("../_lib/BackgroundProcess");
+(async () => {
+  const strapi = await adminStrapi();
 
-const forced = "force" in args;
-const RUNNING_STATUS = Symbol();
+  const queryParams = {
+    status: "published",
+    _limit: 9999,
+    _sort: "id:desc",
+  };
 
-class ProductValidator extends BackgroundProcess {
-  constructor({ forced = false }) {
-    super({
-      baseDir: path.resolve(__dirname),
-    });
+  const sources = await strapi.services.source.find();
+  const foundProducts = await strapi.services.product.find(queryParams);
+  const productsWithIssues = [];
 
-    this.forced = forced;
-    this[RUNNING_STATUS] = false;
-  }
+  // Sources
+  const ebaySource = sources.find(({ name }) => /ebay/i.test(name));
+  const aliexpressSource = sources.find(({ name }) => /ali/i.test(name));
 
-  onSwitchStart() {
-    this.logger.log("Starting validator");
+  console.log(
+    `Running validator on ${foundProducts.length} product(s)...`.cyan
+  );
 
-    if (this[RUNNING_STATUS]) {
-      return;
-    }
+  for (let i = 0; i < foundProducts.length && this[RUNNING_STATUS]; i++) {
+    const product = foundProducts[i];
+    const productIssues = [];
 
-    this[RUNNING_STATUS] = true;
-
-    new Promise((resolve, reject) => {
-      return this.validate().then(resolve).catch(reject);
-    }).catch((err) => {
-      console.log("Validator start error", err);
-    });
-  }
-
-  onSwitchStop() {
-    // Stop validator
-    if (this.running) {
-      this.cancel();
-    } else {
-      this.stop();
-    }
-  }
-
-  onSwitchError() {
-    // Stop validator
-    this.stop();
-  }
-
-  cancel() {
-    this.emit("cancel");
-  }
-
-  stop() {
-    this.logger.log("Stopping validator");
-
-    if (this[RUNNING_STATUS]) {
-      this[RUNNING_STATUS] = false;
-      this.emit("stop");
-    }
-  }
-
-  handleError(err) {
-    if (err.message === "cancel") {
-      if (this.running) {
-        this.logger.log("Validator Cancelled");
-      }
-      this.stop();
-    } else {
-      this.logger.log(
-        `${err.message} ${`STACK`.black.bold} ==> ${err.stack.gray}`,
-        "ERROR"
-      );
-      this.switch.error();
-    }
-  }
-
-  get running() {
-    return this[RUNNING_STATUS];
-  }
-
-  async validate() {
-    new Promise(this.startCancellableValidator.bind(this)).catch(
-      this.handleError.bind(this)
-    );
-  }
-
-  async startCancellableValidator(resolve, reject) {
-    this.on("cancel", () => {
-      // Throw an error in order to cancel current validation process
-      reject(new Error("cancel"));
-    });
-
-    const strapi = await adminStrapi();
-
-    const queryParams = {
-      status: "published",
-      _limit: 9999,
-      _sort: "id:desc",
-    };
-
-    // Include all products if forced is true
-    if (this.forced && queryParams.status) {
-      delete queryParams.status;
-    }
-
-    const sources = await strapi.services.source.find();
-    const foundProducts = await strapi.services.product.find(queryParams);
-    const productsWithIssues = [];
-
-    // Sources
-    const ebaySource = sources.find(({ name }) => /ebay/i.test(name));
-    const aliexpressSource = sources.find(({ name }) => /ali/i.test(name));
-
-    this.logger.log(
-      `Running validator on ${foundProducts.length} product(s)...`.cyan
+    console.log(
+      `[ ${i + 1} of ${foundProducts.length} ]`.cyan.bold +
+        ` Validating - [${String(product.id).bold}] ${product.title}`
     );
 
-    for (let i = 0; i < foundProducts.length && this[RUNNING_STATUS]; i++) {
-      const product = foundProducts[i];
-      const productIssues = [];
-
-      this.logger.log(
-        `[ ${i + 1} of ${foundProducts.length} ]`.cyan.bold +
-          ` Validating - [${String(product.id).bold}] ${product.title}`
+    // Validate amazon link
+    if (!isAmazonLink(product.amazon_url)) {
+      console.error(
+        `Invalid amazon url for [ ${product.id} ] - ${product.title}`
       );
-
-      // Validate amazon link
-      if (!isAmazonLink(product.amazon_url)) {
-        console.error(
-          `Invalid amazon url for [ ${product.id} ] - ${product.title}`
+      productIssues.push("amazon_link_invalid");
+    } else {
+      try {
+        const scrapedDetails = await scrapeAmazonProduct(
+          product.amazon_url,
+          "en",
+          true,
+          true
         );
-        productIssues.push("amazon_link_invalid");
-      } else {
-        try {
-          const scrapedDetails = await scrapeAmazonProduct(
-            product.amazon_url,
-            "en",
-            true,
-            true
-          );
 
-          if (!scrapedDetails) {
-            productIssues.push("amazon_link_unavailable");
-          }
-        } catch (err) {
-          this.logger.log(err.stack, "ERROR");
+        if (!scrapedDetails) {
           productIssues.push("amazon_link_unavailable");
         }
-      }
-
-      // Validate other URLs
-      if (product.url_list && product.url_list.length) {
-        for (const urlData of product.url_list) {
-          switch (urlData.source.id) {
-            // Validate Ebay
-            case ebaySource.id:
-              if (!(await getDetailsFromEbayURL(urlData.url))) {
-                productIssues.push("ebay_link_invalid");
-              }
-              break;
-
-            // Validate AliExpress
-            case aliexpressSource.id:
-              try {
-                if (!(await getDetailsFromAliExppressURL(urlData.url))) {
-                  throw new Error("Invalid AlixExpress Link");
-                }
-              } catch (err) {
-                productIssues.push("aliexpress_link_invalid");
-              }
-              break;
-          }
-        }
-      }
-
-      if (productIssues.length) {
-        product.product_issues = {};
-
-        productIssues.forEach((product_issue) => {
-          product.product_issues[product_issue] = true;
-        });
-
-        // Save product as draft
-        product.status = "draft";
-        await strapi.services.product.updateProduct(
-          product.id,
-          { status: "draft", product_issues: product.product_issues },
-          { price: false, amazonDetails: false },
-          { change_type: "product_validator_results" }
-        );
-
-        this.logger.log(
-          `Issue(s) were found for [`.yellow +
-            String(product.id).yellow.bold +
-            `] ${product.title}`.yellow,
-          "ERROR"
-        );
-        productsWithIssues.push(product.id);
-      } else {
-        // Clear out product issues
-        await strapi.services.product.updateProduct(
-          product.id,
-          { product_issues: null },
-          { price: false, amazonDetails: false },
-          { change_type: "product_validator_results" }
-        );
-        this.logger.log("DONE".green);
+      } catch (err) {
+        console.error(err.stack);
+        productIssues.push("amazon_link_unavailable");
       }
     }
 
-    this.logger.log(" DONE ".bgGreen.white.bold);
-    this.switch.stop();
+    // Validate other URLs
+    if (product.url_list && product.url_list.length) {
+      for (const urlData of product.url_list) {
+        switch (urlData.source.id) {
+          // Validate Ebay
+          case ebaySource.id:
+            if (!(await getDetailsFromEbayURL(urlData.url))) {
+              productIssues.push("ebay_link_invalid");
+            }
+            break;
+
+          // Validate AliExpress
+          case aliexpressSource.id:
+            try {
+              if (!(await getDetailsFromAliExppressURL(urlData.url))) {
+                throw new Error("Invalid AlixExpress Link");
+              }
+            } catch (err) {
+              productIssues.push("aliexpress_link_invalid");
+            }
+            break;
+        }
+      }
+    }
+
+    if (productIssues.length) {
+      product.product_issues = {};
+
+      productIssues.forEach((product_issue) => {
+        product.product_issues[product_issue] = true;
+      });
+
+      // Save product as draft
+      product.status = "draft";
+      await strapi.services.product.updateProduct(
+        product.id,
+        { status: "draft", product_issues: product.product_issues },
+        { price: false, amazonDetails: false },
+        { change_type: "product_validator_results" }
+      );
+
+      console.error(
+        `Issue(s) were found for [`.yellow +
+          String(product.id).yellow.bold +
+          `] ${product.title}`.yellow,
+      );
+      productsWithIssues.push(product.id);
+    } else {
+      // Clear out product issues
+      await strapi.services.product.updateProduct(
+        product.id,
+        { product_issues: null },
+        { price: false, amazonDetails: false },
+        { change_type: "product_validator_results" }
+      );
+      console.log("DONE".green);
+    }
   }
-}
 
-ProductValidator.backgroundProcessName = "Product Validator";
-
-const productValidator = new ProductValidator({ forced });
-
-module.exports = productValidator;
+  console.log(" DONE ".bgGreen.white.bold);
+  process.exit();
+})();
--- admin/scheduled-tasks/config/_tasks.js
+++ admin/scheduled-tasks/config/_tasks.js
@@ -1,12 +1,11 @@
-const path = require("path");
 const frequencies = require("./_frequencies");
 
 module.exports = [
-  // {
-  //   id: "product-validator",
-  //   name: "Product Validator",
-  //   schedule: frequencies.daily / 2,
-  // },
+  {
+    id: "product-validator",
+    name: "Product Validator",
+    schedule: frequencies.daily,
+  },
   {
     id: "product-price-updater",
     name: "Product Price Updater",
