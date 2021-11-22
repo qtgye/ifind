0eb3c397ff5cc96e6e62a49829cebbf42acbf1fa
qtgye <buquia.jace@gmail.com>
2021-11-22 22:08:36 +0800

    Admin - TorProxy Updates

--- admin/helpers/tor-proxy.js
+++ admin/helpers/tor-proxy.js
@@ -3,9 +3,12 @@
  * - Tor service configured as proxy server (source: https://levelup.gitconnected.com/anonymous-web-scrapping-with-node-js-tor-apify-and-cheerio-3b36ec6a45dc)
  * - Tor config set to german country (source: https://ab-kotecha.medium.com/how-to-connect-from-a-specific-country-without-any-vpn-but-privately-enough-through-tor-browser-5dc2d45043b)
  */
-require('colors');
+
+require("colors");
 const { existsSync, readFileSync } = require("fs-extra");
 const puppeteer = require("puppeteer");
+
+// Parse Tor config file path provided in the .env
 const { TORRC_PATH } = require("dotenv").config().parsed;
 
 if (!TORRC_PATH || !existsSync(TORRC_PATH)) {
@@ -14,6 +17,7 @@ if (!TORRC_PATH || !existsSync(TORRC_PATH)) {
   );
 }
 
+// Parse socksport configuration from the torrc file
 const portsConfig = readFileSync(TORRC_PATH)
   .toString()
   .match(/socksport\s+\d+/gi);
@@ -21,30 +25,92 @@ const availablePorts = portsConfig
   .map((portConfig) => portConfig.split(/\s+/)[1])
   .filter(Boolean);
 
-const getProxiedBrowserPage = async () => {
-  const randomIndex = Math.round(
-    Math.random() * (availablePorts.length - 1)
-  );
+class TorProxy {
+  /**
+   * Creates a new instance of this class
+   */
+  static create() {
+    return new TorProxy();
+  }
 
-  const port = availablePorts[randomIndex] ;
-  const proxy = `--proxy-server=socks5://127.0.0.1:${port}`;
+  /**
+   * Generates a new page using the current browser
+   * @return Puppeteer Page
+   */
+  async newPage(newBrowser = false) {
 
-  console.log(`\nLanching browser using Tor Proxy port: ${port.bold}`)
+    /* Ensure a browser instance is present */
+    if (!this.browser) {
+      await this.launchNewBrowser();
+    }
 
-  const browser = await puppeteer.launch({
-    args: [proxy, '--no-sandbox'],
-  });
+    /* Close all other pages/tabs */
+    this.closePages();
 
-  const page = await browser.newPage();
+    /* Log */
+    console.info(`Getting new page.`);
 
-  await page.setViewport({
-    width: 1920,
-    height: 3000,
-  });
+    /* Create new Page */
+    const page = await this.browser.newPage();
 
-  return page;
-};
+    /* Apply desktop breakpoint */
+    await page.setViewport({
+      width: 1920,
+      height: 3000,
+    });
 
-module.exports = {
-  getProxiedBrowserPage,
-};
+    /* Return new page */
+    return page;
+  }
+
+  /**
+   * Launches a new browser with proxy settings
+   */
+  async launchNewBrowser() {
+
+    /* Get proxy */
+    const proxy = this.generateProxy();
+
+    /* Log */
+    console.info(`Launching new browser using proxy: ${proxy.bold}`);
+
+    /* Launching browser */
+    this.browser = await puppeteer.launch({
+      args: [`--proxy-server=${proxy}`, "--no-sandbox"],
+    });
+
+    /* Nullify reference to browser if disconnected */
+    this.browser.on("disconnected", () => {
+      this.browser = null;
+    });
+  }
+
+  /**
+   * Generates a proxy to be used for the Puppeteer browser launch
+   * e.g., socks5://127.0.0.1:9050
+   * @returns String ()
+   */
+  generateProxy() {
+    const port = this.getSocksPort();
+    return `socks5://127.0.0.1:${port}`;
+  }
+
+  /**
+   * Selects a port from the Torrc SocksPorts list
+   * @returns Number
+   */
+  getSocksPort() {
+    const randomIndex = Math.round(Math.random() * (availablePorts.length - 1));
+    return availablePorts[randomIndex];
+  }
+
+  /**
+   * Close all current pages
+   */
+  async closePages() {
+    const pages = await this.browser.pages();
+    await Promise.all(pages.map((page) => page.close()));
+  }
+}
+
+module.exports = () => TorProxy.create();
--- admin/test.js
+++ admin/test.js
@@ -1,12 +1,9 @@
 require("colors");
-const {
-  ensureDirSync,
-  rmdirSync,
-} = require("fs-extra");
+const { ensureDirSync, rmdirSync } = require("fs-extra");
 const path = require("path");
 
 const { addURLParams } = require("./helpers/url");
-const { getProxiedBrowserPage } = require('./helpers/tor-proxy');
+const createTorProxy = require("./helpers/tor-proxy");
 
 const LIGHTNING_OFFERS_PAGE =
   "https://www.amazon.de/-/en/gp/angebote?ref_=nav_cs_gb_c869dbce88784497bfc3906e5456094e&deals-widget=%257B%2522version%2522%253A1%252C%2522viewIndex%2522%253A0%252C%2522presetId%2522%253A%2522deals-collection-lightning-deals%2522%252C%2522dealType%2522%253A%2522LIGHTNING_DEAL%2522%252C%2522sorting%2522%253A%2522BY_SCORE%2522%257D";
@@ -29,6 +26,8 @@ const PRICE_SELECTOR = [
 ].join(",");
 
 (async () => {
+  const torProxy = createTorProxy();
+
   // Clear screenshots root
   try {
     rmdirSync(SCREENSHOTS_ROOT, {
@@ -41,13 +40,13 @@ const PRICE_SELECTOR = [
   ensureDirSync(SCREENSHOTS_ROOT);
 
   // Get products list
-  console.log("Getting products list...");
-  const page = await getProxiedBrowserPage();
-  await page.goto(LIGHTNING_OFFERS_PAGE, {
+  console.info("Getting products list...");
+  let proxiedPage = await torProxy.newPage();
+  await proxiedPage.goto(LIGHTNING_OFFERS_PAGE, {
     timeout: 60000 * 3,
   });
-  await page.waitForSelector(PRODUCT_CARD);
-  const urls = await page.evaluate((PRODUCT_CARD) => {
+  await proxiedPage.waitForSelector(PRODUCT_CARD);
+  const urls = await proxiedPage.evaluate((PRODUCT_CARD) => {
     const productCards = Array.from(document.querySelectorAll(PRODUCT_CARD));
     return productCards
       .map((card) => {
@@ -57,51 +56,54 @@ const PRICE_SELECTOR = [
       .filter((url) => /amazon\.[a-z]+\/[^\/]{10,}\//.test(url));
   }, PRODUCT_CARD);
 
-  await page.screenshot({
-    path: path.resolve(SCREENSHOTS_ROOT, 'index.png'),
+  await proxiedPage.screenshot({
+    path: path.resolve(SCREENSHOTS_ROOT, "index.png"),
   });
 
-  console.log(`Got a list of ${urls.length} products.`);
+  console.info(`Got a list of ${urls.length} products.`);
 
   let products = 0;
   let urlIndex = 0;
   while (products < 30) {
     const timeStart = Date.now();
-    let page = await getProxiedBrowserPage();
 
     // Go to page
-    console.log(`[ ${String(products + 1).bold} ] Going to product page...`);
+    console.info(`\n[ ${String(products + 1).bold} ] Going to product page...`);
     const url = urls[urlIndex++];
     const urlEnglish = addURLParams(url, { language: "en" });
-    console.log(urlEnglish.cyan);
+    console.info(urlEnglish.cyan);
 
     try {
       let hasPriceElement = false;
       let tries = 1;
 
-      while (!hasPriceElement && 3 > tries++ ) {
+      while (!hasPriceElement && tries <= 3) {
         try {
-          await page.goto(urlEnglish, {
+          proxiedPage = await torProxy.newPage();
+          await proxiedPage.goto(urlEnglish, {
             timeout: 60000 * 3,
           });
-          await page.waitForSelector(PRICE_SELECTOR);
+          await proxiedPage.waitForSelector(PRICE_SELECTOR);
           hasPriceElement = true;
-        }
-        catch (err) {
-          console.log(err.message.magenta);
-          console.log('Retrying...'.yellow);
-          page = await getProxiedBrowserPage();
+        } catch (err) {
+          console.info(err.message.magenta);
+          console.info("Retrying...".yellow);
+          if (tries <= 3) {
+            await torProxy.launchNewBrowser();
+            proxiedPage = await torProxy.newPage();
+          }
+          tries++;
         }
       }
 
-      if ( !hasPriceElement ) {
-        throw new Error('Page might have error');
+      if (!hasPriceElement) {
+        throw new Error("Page might have error");
       }
     } catch (err) {
       console.error(err.message.magenta);
     }
 
-    console.log("Saving Screenshot");
+    console.info("Saving Screenshot");
     const screenShortDir = path.resolve(
       SCREENSHOTS_ROOT,
       url.replace("https://www.amazon.de/", "")
@@ -109,24 +111,29 @@ const PRICE_SELECTOR = [
     ensureDirSync(screenShortDir);
 
     // Remove cookie banner
-    await page.evaluate(() => {
-      const cookieBanner = document.querySelector('#sp-cc');
-      if ( cookieBanner ) {
+    await proxiedPage.evaluate(() => {
+      const cookieBanner = document.querySelector("#sp-cc");
+      if (cookieBanner) {
         cookieBanner.remove();
       }
     });
 
     // Save screenshot
-    await page.screenshot({
+    await proxiedPage.screenshot({
       path: path.resolve(screenShortDir, "index.png"),
     });
 
-    (await page.browser()).close();
     products++;
 
     const timeEnd = Date.now();
-    console.log(`Time spent: ${Number(((timeEnd - timeStart) / 1000).toFixed(2))} seconds.`.italic.bold);
+    console.info(
+      `Time spent: ${Number(
+        ((timeEnd - timeStart) / 1000).toFixed(2)
+      )} seconds.`.italic.bold
+    );
   }
 
-  console.log("DONE".bold.green);
+  console.info("DONE".bold.green);
+
+  process.exit();
 })();
