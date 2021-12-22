76a1a805d2fd44f494c71efd14b9f7f449e6bf07
qtgye <buquia.jace@gmail.com>
2021-12-22 18:55:06 +0800

    Sidenav closed by default, German language for scraped products

--- front-end/src/components/Header/HeaderSideNav.tsx
+++ front-end/src/components/Header/HeaderSideNav.tsx
@@ -26,7 +26,7 @@ const HeaderSideNav = () => {
   const { dealTypeName, onOffersClick } = useContext(GlobalStateContext);
 
   const listRef = useRef<HTMLDivElement | null>();
-  const [isVisible, setIsVisible] = useState(true);
+  const [isVisible, setIsVisible] = useState(false);
   const [checked, setChecked] = useState(false);
   const checkChange = () => setChecked(!checked);
 
