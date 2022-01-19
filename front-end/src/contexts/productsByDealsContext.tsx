02968a02565284b86737ee79181da5a2b4d524ee
qtgye <buquia.jace@gmail.com>
2022-01-19 20:55:20 +0800

    FE - Fix linter warning

--- front-end/src/contexts/productsByDealsContext.tsx
+++ front-end/src/contexts/productsByDealsContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, useEffect, useCallback } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { useQuery } from "@apollo/react-hooks";
 import productsByDealsQuery from "@gql/productsByDealsQuery";
 
