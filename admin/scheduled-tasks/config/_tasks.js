0a2294b094fa534eb6a8c25532b4cdece5560f08
qtgye <buquia.jace@gmail.com>
2022-01-27 22:06:52 +0800

    Admin - Add Scheduled Tasks Deal Type keys

--- admin/scheduled-tasks/config/_tasks.js
+++ admin/scheduled-tasks/config/_tasks.js
@@ -15,15 +15,24 @@ module.exports = [
     id: "amazon-lightning-offers",
     name: "Amazon Lightning Offers",
     schedule: frequencies.hourly,
+    meta: {
+      deal_type: "amazon_flash_offers",
+    },
   },
   {
     id: "ebay-wow-offers",
     name: "Ebay Wow Offers",
     schedule: frequencies.hourly,
+    meta: {
+      deal_type: "ebay_wow_offers",
+    },
   },
   {
     id: "aliexpress-value-deals",
     name: "AliExpress Super Value Deals",
     schedule: frequencies.hourly,
+    meta: {
+      deal_type: "aliexpress_value_deals",
+    },
   },
 ];
