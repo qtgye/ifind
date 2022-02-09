81fff4d4d7b7e52a90cf97d2b0fe49d9c0d6f328
KuLaK <dnadragon@gmx.net>
2022-02-09 16:25:53 +0100

    Translation for the contact and offers page

--- front-end/src/components/ProductDealsGrid/translations.ts
+++ front-end/src/components/ProductDealsGrid/translations.ts
@@ -1,8 +1,12 @@
 export const updatedTime = {
   en: "Updated {TIME} ago",
-  de: "Aktualisiert Vor {TIME}",
+  de: "Aktualisiert vor {TIME}",
+  ru: "Обновлено {TIME}",
 };
 
 export const loadMoreButton = {
-  en: 'Load More Offers',
+  en: 'Load further offers',
+  de: 'Weitere Angebote laden',
+  ru: 'Загрузить дополнительные предложения',
+
 }
