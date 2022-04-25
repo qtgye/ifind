c02fb08f7f03001fd07f07bed1a99cd75aa7dab2
qtgye <buquia.jace@gmail.com>
2022-04-25 21:46:13 +0800

    Add check for page-errors folder

--- admin/helpers/amazon/clearPageErrors.js
+++ admin/helpers/amazon/clearPageErrors.js
@@ -2,5 +2,8 @@ const path = require("path");
 const fs = require("fs-extra");
 const pageErrorsDir = path.resolve(__dirname, "page-errors");
 
-module.exports = () =>
-  fs.rmdirSync(pageErrorsDir, { force: true, recursive: true });
+module.exports = () => {
+  if ( fs.existsSync(pageErrorsDir) ) {
+    fs.rmdirSync(pageErrorsDir, { force: true, recursive: true });
+  }
+}
