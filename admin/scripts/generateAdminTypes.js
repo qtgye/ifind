6356a72d2f9c522308a4b4162dab3cef072b7034
qtgye <buquia.jace@gmail.com>
2022-04-26 17:37:51 +0800

    Ensure typings folder exists

--- admin/scripts/generateAdminTypes.js
+++ admin/scripts/generateAdminTypes.js
@@ -1,11 +1,14 @@
 require("colors");
 const path = require("path");
 const glob = require("glob");
-const { readFileSync } = require("fs-extra");
+const { readFileSync, ensureDirSync } = require("fs-extra");
 const { generateTypeScriptTypes } = require("graphql-schema-typescript");
 
 const projectRoot = path.resolve(__dirname, "../");
 const outputPath = path.resolve(projectRoot, "typings/admin.graphql.d.ts");
+
+// Ensure typings folder exists
+ensureDirSync(path.resolve(projectRoot, "typings"));
 
 module.exports = async () => {
   const [schemaFile] = glob.sync(
@@ -25,11 +28,12 @@ module.exports = async () => {
 
   generateTypeScriptTypes(schemaContents, outputPath, options)
     .then(() => {
-      console.log("Admin GraphQL Schema has been exported as TS Typed Definitions.".green);
+      console.log(
+        "Admin GraphQL Schema has been exported as TS Typed Definitions.".green
+      );
     })
     .catch((err) => {
       console.error(err);
       process.exit(1);
     });
-}
-
+};
