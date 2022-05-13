859be388b05b4e8f12760f0fcdc0d6968655f9d4
dev1302 <customer.success@blucursor.com>
2022-05-13 19:00:35 +0530

    MAe changes according to Script server api paths and remove extra code - 13/05/2022

--- admin/scheduled-tasks/tasks/amazon-lightning-offers/index.js
+++ admin/scheduled-tasks/tasks/amazon-lightning-offers/index.js
@@ -1,23 +1,14 @@
 const createStrapiInstance = require("../../../scripts/strapi-custom");
 const { default: axios } = require("axios");
 
-const RETRY_WAIT = 10000;
-const DEAL_TYPE = "amazon_flash_offers";
-const PRODUCTS_TO_SCRAPE = null;
 
 (async () => {
-  const strapi = await createStrapiInstance();
-  const [aliexpressSource, germanRegion] = await Promise.all([
-    strapi.services.source.findOne({ name_contains: "aliexpress" }),
-    strapi.services.region.findOne({ code: "de" }),
-  ]);
 
   try {
     let scrapedProducts = null
-    await axios.post("http://localhost:3000/aliexpress/getAliExpressData").then(
+    await axios.post("http://164.90.181.113:3000/amazon/getAmazonProducts").then(
       (response) => {
-        scrapedProducts = response.data.data;
-        // offers.push(response.data)
+        scrapedProducts = response.data;
       },
       (error) => {
         console.log(error);
@@ -26,32 +17,8 @@ const PRODUCTS_TO_SCRAPE = null;
 
     // Remove old products
     console.log("Removing old products...".green);
-    const deletedProducts = await strapi.services.product.delete({
-      deal_type: DEAL_TYPE,
-    });
-    console.log(`Deleted ${deletedProducts.length} product(s).`.cyan);
 
     console.info(`Saving scraped products...`.bold);
-
-    // Counter
-    let savedProducts = 0;
-
-    for (const productData of scrapedProducts) {
-      // Save product
-      try {
-        const newProduct = await strapi.query("product").create(productData);
-        console.log(
-          `[ ${++savedProducts} of ${scrapedProducts.length} ] Saved new product: ${newProduct.title.bold
-            }`.green
-        );
-      } catch (err) {
-        console.error(err.message);
-      }
-    }
-    // }
-    // else {
-    //   console.log('No products were fetched.'.red.bold);
-    // }
 
     console.log(" DONE ".bgGreen.white.bold);
     productScraper.close();
--- admin/scheduled-tasks/tasks/ebay-wow-offers/index.js
+++ admin/scheduled-tasks/tasks/ebay-wow-offers/index.js
@@ -12,7 +12,7 @@ const EBAY_DEAL_TYPE = "ebay_wow_offers";
     console.log("Getting Ebay Wow Offers...");
     // const offers = await getEbayWowOffers();
     let offers = null
-    await axios.post("http://localhost:3000/ebay/fetchEbayStore").then(
+    await axios.post("http://164.90.181.113:3000/ebay/fetchEbayStore").then(
       (response) => {
         offers = response.data.data;
         // offers.push(response.data)
@@ -21,50 +21,11 @@ const EBAY_DEAL_TYPE = "ebay_wow_offers";
         console.log(error);
       }
     );
-    console.log("offers", offers);
-    const strapi = await createStrapiInstance();
-    const [ebaySource, germanRegion] = await Promise.all([
-      strapi.services.source.findOne({ name_contains: "ebay" }),
-      strapi.services.region.findOne({ code: "de" }),
-    ]);
 
-    // Remove old products
-    console.log("Removing old products...".green);
-    const deletedProducts = await strapi.services.product.delete({
-      deal_type: EBAY_DEAL_TYPE,
-    });
-    console.log(`Deleted ${deletedProducts.length} product(s).`.cyan);
+    console.log(`Deleted  product(s).`.cyan);
 
     console.log("Saving new products...");
     let savedProducts = 0;
-
-    for (const offer of offers) {
-      // Add strapi-specific data
-      const newProduct = {
-        website_tab: 'home',
-        deal_type: EBAY_DEAL_TYPE,
-        title: offer.title,
-        image: offer.image,
-        deal_quantity_available_percent: offer.quantity_available_percent,
-        url_list: [
-          {
-            source: ebaySource.id,
-            region: germanRegion.id,
-            url: ebayLink(offer.url),
-            price: offer.price,
-            price_original: offer.price_original,
-            discount_percent: offer.discount_percent,
-            quantity_available_percent: offer.quantity_available_percent,
-          },
-        ],
-      };
-
-      await strapi.services.product.create(newProduct);
-      console.log(
-        `[ ${++savedProducts} of ${offers.length} ] Saved new product: ${newProduct.title
-          }`.green
-      );
-    }
 
     console.log(" DONE ".bgGreen.white.bold);
     process.exit();
