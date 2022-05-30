b61075da4fad065e019cc7f7b7128584169f1e1a
dev1302 <customer.success@blucursor.com>
2022-05-30 19:31:53 +0530

    Add scheduled task list api in productByDeals | 30/05/2022

--- admin/api/ifind/services/methods/productsByDeals.js
+++ admin/api/ifind/services/methods/productsByDeals.js
@@ -1,6 +1,19 @@
 const dealTypes = appRequire("api/ifind/deal-types");
 const offersCategories = appRequire("api/ifind/offers-categories");
 const axios = require('axios').default;
+var request = require('request');
+var agentOptions;
+var agent;
+'use strict';
+
+agentOptions = {
+  host: 'https://script.ifindilu.de'
+, port: '443'
+, path: '/'
+, rejectUnauthorized: false
+};
+
+agent = new https.Agent(agentOptions);
 
 const PRODUCTS_PER_PAGE = 999999;
 
@@ -8,11 +21,21 @@ module.exports = async ({ deal_type = "", start = 0, offer_category = "" }) => {
   const sources = await strapi.services.source.find();
   // const scheduledTasks = await strapi.scheduledTasks.list();
   const scheduledTasks = null
-    await axios.post("https://script.ifindilu.de/task/getTaskList")
-      .then((response) => {
-         scheduledTasks = response.data.tasks
-      })
-      .catch((err) => console.log("error ", err.code))
+    // await axios.post("https://script.ifindilu.de/task/getTaskList")
+    //   .then((response) => {
+    //      scheduledTasks = response.data.tasks
+    //   })
+    //   .catch((err) => console.log("error ", err.message))
+    request({
+      url: "https://script.ifindilu.de/task/getTaskList"
+    , method: 'POST'
+    , agent: agent
+    }, function (err, resp, body) {
+      // ...
+      scheduledTasks = resp.data.tasks
+      if(err) console.log("Error : err");
+    });
+
 
   const defaultOffersCategory = Object.keys(offersCategories).find(
     (categoryKey) => offersCategories[categoryKey].isDefault
