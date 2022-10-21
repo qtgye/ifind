cc9f9acfd2cb2dc41ee074a505e54de6717519a5
Jayson Buquia <buquia.jace@gmail.com>
2022-10-21 12:47:23 +0800

    Save new products by chunks

--- admin/api/ifind/services/methods/addProductsByDeals.js
+++ admin/api/ifind/services/methods/addProductsByDeals.js
@@ -1,3 +1,4 @@
+require('colors');
 const deleteProductsByDeals = require("./deleteProductsByDeals");
 
 /**
@@ -8,11 +9,29 @@ module.exports = async ({ deal_type, products }) => {
   await deleteProductsByDeals({ deal_type });
 
   // Add new products
-  const newProducts = await Promise.all(
-    products.map(
-      async (productData) => await strapi.services.product.create(productData)
-    )
-  );
+  // Save products by chunks of 10, so the system won't crash
+  const productChunks = [];
+  const newProducts = [];
+
+  products.forEach((product, index) => {
+    if (index % 10 === 0) {
+      productChunks.push([]);
+    }
+
+    productChunks[productChunks.length - 1].push(product);
+  });
+
+  for (productsGroup of productChunks) {
+    const addedProducts = await Promise.all(
+      productsGroup.map(
+        async (productData) => await strapi.services.product.create(productData)
+      )
+    );
+
+    newProducts.push(...addedProducts);
+  }
+
+  console.info(`Added ${newProducts.length} new products.`.bold.green);
 
   return newProducts;
 };
