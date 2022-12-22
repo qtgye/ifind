b3aaa52e35dc977af7c163fe021b451ba5af5cbc
qtgye <buquia.jace@gmail.com>
2022-12-22 16:55:35 +0800

    Exclude products from disabled dealTypes

--- admin/api/ifind/services/methods/productsByDeals.js
+++ admin/api/ifind/services/methods/productsByDeals.js
@@ -52,7 +52,9 @@ module.exports = async ({ deal_type = "", start = 0, offer_category = "" }) => {
     : dealCategories[defaultOffersCategory];
   const selectedDealTypes = offerCategory
     ? offerCategory.dealTypes.reduce((dealTypesMap, dealTypeKey) => {
-        dealTypesMap[dealTypeKey] = dealTypes[dealTypeKey];
+        if (!dealTypes[dealTypeKey].disabled) {
+          dealTypesMap[dealTypeKey] = dealTypes[dealTypeKey];
+        }
         return dealTypesMap;
       }, {})
     : null;
